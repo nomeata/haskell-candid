2a15d44beba7462aebc860761c4eac9cbbd9a202
Joachim Breitner <mail@joachim-breitner.de>
2020-06-04 18:42:09 +0200

    More tweaks

--- cmdline.hs
+++ cmdline.hs
@@ -2,6 +2,7 @@
 
 import Data.Text.Prettyprint.Doc (pretty)
 import qualified Data.Text as T
+import qualified Data.Vector as V
 import Options.Applicative
 import Control.Monad (join)
 import System.IO
@@ -32,25 +33,39 @@ main = join . customExecParser (prefs showHelpOnError) $
         <*> optional ( strArgument ( metavar "METHOD" ) )
         <*> many ( strArgument ( metavar "ARGS..." ) )
 
+val :: Type Void -> (String, ReadM Value)
+val TextT = ("STR", TextV <$> str)
+val NatT = ("NAT", NatV <$> auto)
+val t = ("CANDID", eitherReader parseValue)
+
+plainFieldArg :: FieldName -> Type Void -> Parser Value
+plainFieldArg fn t = option p (long (show (pretty fn)) <> metavar mv <> help ("type: " <> show (pretty t)))
+  where (mv, p) = val t
+
+fieldArg :: FieldName -> Type Void -> Parser Value
+fieldArg fn BoolT = BoolV <$> switch (long (show (pretty fn)) <> help "type: bool")
+fieldArg fn (VecT t) = VecV . V.fromList <$> many (plainFieldArg fn t)
+fieldArg fn (OptT t) = OptV <$> optional (plainFieldArg fn t)
+fieldArg fn t = plainFieldArg fn t
+
 field :: (FieldName, Type Void) -> Parser (FieldName, Value)
-field (fn, TextT) = (fn,) . TextV <$> strOption (long (show (pretty fn)) <> metavar "STR")
-field (fn, NatT) = (fn,) . NatV <$> option auto (long (show (pretty fn)) <> metavar "NAT")
+field (fn, t) = (fn,) <$> fieldArg fn t
 
 arg :: Type Void -> Parser Value
-arg TextT = TextV <$> strArgument ( metavar "STR" )
 arg (RecT fs) = RecV <$> traverse field fs
+arg t = argument p ( metavar mv <> help ("type: " <> show (pretty t)) )
+  where (mv, p) = val t
 
 args :: [Type Void] -> Parser [Value]
-args [] = pure []
-args [t] = (:[]) <$> arg t
-args xs = error "Only unary functions supported for now"
+args = traverse arg
 
 service :: DidFile -> Parser (T.Text, [Value])
 service did = hsubparser $ mconcat
-    [ command (T.unpack m) (info ((m,) <$> args a) mempty)
+    [ command (T.unpack m) $ info ((m,) <$> args a) $
+        progDesc ("Invoke " <> T.unpack m)
+        <> fullDesc
     | (m, a, _) <- did
     ]
-
 
 work :: String -> Maybe String -> [String] -> IO ()
 work file method_name args = do
@@ -63,7 +78,9 @@ work file method_name args = do
         Right did -> return did
 
     (m,x) <- handleParseResult $ execParserPure
-        (prefs showHelpOnError)
-        (info (helper <*> service did) fullDesc)
+        (prefs (showHelpOnError <> showHelpOnEmpty))
+        (info (helper <*> service did) (
+            fullDesc <> progDesc "Invoke methods on a canister"
+        ))
         (toList method_name ++ args)
     print $ pretty (m,x)
