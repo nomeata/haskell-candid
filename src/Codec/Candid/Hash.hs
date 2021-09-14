0bdb1caeb6c6cd60f42bd591342675654a1cf589
Joachim Breitner <mail@joachim-breitner.de>
2021-09-14 19:42:34 +0200

    Remove unused imports

--- src/Codec/Candid/Hash.hs
+++ src/Codec/Candid/Hash.hs
@@ -10,12 +10,10 @@ module Codec.Candid.Hash
 
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
-import qualified Data.Text.IO as T
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.IntMap as M
 import Data.Maybe
 import Data.Char
-import Language.Haskell.TH.Syntax
 import Data.Word
 import Data.FileEmbed
 
