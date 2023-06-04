accf1a5621a55a24d088b03bdf72ca03a769e664
Joachim Breitner <mail@joachim-breitner.de>
2023-06-04 14:58:48 +0200

    GHC 9.6 (#25)
    * Bump base32
    
    * Also bump stackage nightly
    
    * GHC 9.6 compatibility
    
    * Bump haskell action
    
    * Bounds
    
    * More action bumps
    
    * Allow newer stuff
    
    * Allow more new stuff
    
    * Yuck, more dependencies
    
    * Use cabal-plan-bounds --extend
    
    * Update cabal bounds based on build plans
    
    ---------
    
    Co-authored-by: nomeata <nomeata@users.noreply.github.com>

--- src/Codec/Candid/Generic.hs
+++ src/Codec/Candid/Generic.hs
@@ -14,6 +14,7 @@ module Codec.Candid.Generic (AsRecord(..), AsVariant(..)) where
 import qualified Data.Row as R
 import qualified Data.Row.Records as R
 import qualified Data.Row.Variants as V
+import qualified Data.Row.Dictionaries as R
 import Data.Typeable
 
 import Codec.Candid.Class
@@ -27,6 +28,12 @@ type CanBeCandidRecord a =
     , Candid (R.Rec (R.NativeRow a))
     , R.ToNative a
     , R.FromNative a
+    -- Superclass constraints that need to be explicit since GHC 9.6
+    , Typeable (R.NativeRow a)
+    , V.AllUniqueLabels (R.NativeRow a)
+    , V.AllUniqueLabels (V.Map (Either String) (R.NativeRow a))
+    , V.Forall (R.NativeRow a) R.Unconstrained1
+    , V.Forall (R.NativeRow a) Candid
     )
 instance CanBeCandidRecord a => Candid (AsRecord a) where
     type AsCandid (AsRecord a) = AsCandid (R.Rec (R.NativeRow a))
@@ -41,8 +48,13 @@ type CanBeCandidVariant a =
     , Candid (V.Var (V.NativeRow a))
     , V.ToNative a
     , V.FromNative a
+    -- Superclass constraints that need to be explicit since GHC 9.6
+    , Typeable (V.NativeRow a)
+    , V.AllUniqueLabels (V.NativeRow a)
+    , V.AllUniqueLabels (V.Map (Either String) (V.NativeRow a))
+    , V.Forall (V.NativeRow a) R.Unconstrained1
+    , V.Forall (V.NativeRow a) Candid
     )
-
 instance CanBeCandidVariant a => Candid (AsVariant a) where
     type AsCandid (AsVariant a) = AsCandid (V.Var (V.NativeRow a))
     toCandid = toCandid @(V.Var (V.NativeRow a)) . V.fromNative . unAsVariant
